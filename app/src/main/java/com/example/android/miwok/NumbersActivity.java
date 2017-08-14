7322d0946ff7f90ca9fc116e6735a9338fac4369
Tatchagon Koonkoei <o_k_t@hotmail.com>
2017-08-15 00:07:10 +0700

    Add TextView to Layout with While Loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -25,15 +27,15 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        Log.v("NumnersActivity", "Word at index 0: " + words.get(0));
-        Log.v("NumnersActivity", "Word at index 1: " + words.get(1));
-        Log.v("NumnersActivity", "Word at index 2: " + words.get(2));
-        Log.v("NumnersActivity", "Word at index 3: " + words.get(3));
-        Log.v("NumnersActivity", "Word at index 4: " + words.get(4));
-        Log.v("NumnersActivity", "Word at index 5: " + words.get(5));
-        Log.v("NumnersActivity", "Word at index 6: " + words.get(6));
-        Log.v("NumnersActivity", "Word at index 7: " + words.get(7));
-        Log.v("NumnersActivity", "Word at index 8: " + words.get(8));
-        Log.v("NumnersActivity", "Word at index 9: " + words.get(9));
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        int count = 0;
+        while (count < words.size()){
+            TextView wordTextView = new TextView(this);
+            wordTextView.setText(words.get(count));
+            rootView.addView(wordTextView);
+            count++;
+        }
+
     }
 }
