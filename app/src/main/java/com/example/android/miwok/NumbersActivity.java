c7861f38e5f121889819598941cb75d2c927f1c2
Tatchagon Koonkoei <o_k_t@hotmail.com>
2017-08-14 17:58:51 +0700

    Verify Array Elements with Log Messages

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -21,5 +22,16 @@ public class NumbersActivity extends AppCompatActivity {
         words[7] = "eight";
         words[8] = "nine";
         words[9] = "ten";
+
+        Log.v("NumnersActivity", "Word at index 0: " + words[0]);
+        Log.v("NumnersActivity", "Word at index 1: " + words[1]);
+        Log.v("NumnersActivity", "Word at index 2: " + words[2]);
+        Log.v("NumnersActivity", "Word at index 3: " + words[3]);
+        Log.v("NumnersActivity", "Word at index 4: " + words[4]);
+        Log.v("NumnersActivity", "Word at index 5: " + words[5]);
+        Log.v("NumnersActivity", "Word at index 6: " + words[6]);
+        Log.v("NumnersActivity", "Word at index 7: " + words[7]);
+        Log.v("NumnersActivity", "Word at index 8: " + words[8]);
+        Log.v("NumnersActivity", "Word at index 9: " + words[9]);
     }
 }
