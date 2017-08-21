b93624f693ac34fbe87dff0da0f6e3b9a0a909de
Tatchagon Koonkoei <o_k_t@hotmail.com>
2017-08-21 15:28:32 +0700

    The toString() Method (Optional)

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -91,4 +91,16 @@ public class Word {
         return this.mAudioResourceID;
     }
 
+    /**
+     * @return the string representation of the {@link Word} object.
+     */
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mImageResourceID=" + mImageResourceID +
+                ", mAudioResourceID=" + mAudioResourceID +
+                '}';
+    }
 }
