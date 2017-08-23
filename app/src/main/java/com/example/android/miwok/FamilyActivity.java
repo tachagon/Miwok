e373aba108b45d9b0eed923a8986886dffcd6618
Tatchagon Koonkoei <o_k_t@hotmail.com>
2017-08-23 15:05:32 +0700

    MediaPlayer And Activity Lifecycle

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -70,6 +70,14 @@ public class FamilyActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing anymore sounds.
+        releaseMediaPlayer();
+    }
+
     /**
      * Clean up the media player by releasing its resources.
      */
