ee2f4fc04cf8f74f2b72e5a62c67188180c9431e
Tatchagon Koonkoei <o_k_t@hotmail.com>
2017-08-30 23:45:46 +0700

    Add Tabs to ViewPager

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,21 +1,41 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 /**
- * Provides the appropriate {@link Fragment} for a view pager.
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fm) {
+    final int PAGE_COUNT = 4;
+
+    /**
+     * Context of the app
+     */
+    private Context mContext;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param context is the context of the app
+     * @param fm      is the fragment manager that will keep each fragment's state in the adapter
+     *                across swipes.
+     */
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
     @Override
     public Fragment getItem(int position) {
-        if (position == 0){
+        if (position == 0) {
             return new NumbersFragment();
         } else if (position == 1) {
             return new FamilyFragment();
@@ -26,9 +46,24 @@ public class CategoryAdapter extends FragmentPagerAdapter {
         }
     }
 
+    /**
+     * Return the total number of pages.
+     */
     @Override
     public int getCount() {
-        return 4;
+        return PAGE_COUNT;
     }
 
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
+    }
 }
